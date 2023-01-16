64fbc4b45691180cde924482c824ed3d7fd0ad52
Elif Karagöz <75577514+eliffkaragoz@users.noreply.github.com>
2023-01-16 23:37:48 +0300

    fix : bookings screen

--- client/lib/app/views/bookings/bookings.view.dart
+++ client/lib/app/views/bookings/bookings.view.dart
@@ -1,10 +1,5 @@
 import 'package:client/app/views/bookings/bookings.widgets.dart';
-import 'package:client/app/views/bookings/widgets/tabbar/tabbar_list.dart';
-import 'package:client/app/views/bookings/widgets/tabbar_view/tabbar_view_list.dart';
-import 'package:client/app/widgets/custom_appbar.dart';
-import 'package:client/app/widgets/tabbar/custom_tabbar.dart';
 import 'package:client/core/base/base_view/base_view.dart';
-import 'package:client/core/constans/text_constants.dart';
 import 'package:flutter/material.dart';
 
 class BookingsView extends BaseView with BookingsWidgets {
@@ -21,20 +16,8 @@ class BookingsView extends BaseView with BookingsWidgets {
     return dynamicBuild(
       context,
       error: backendError(),
-      appbar: CustomAppbar(
-          leading: Text(
-        "Your Bookings",
-        style: TextConstants.instance.heading6,
-      )),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CustomTabBar(
-            tabBarList: tabBarList,
-            tabBarViewList: tabBarViewList,
-          ),
-        ],
-      ),
+      appbar: appBar(),
+      body: body(),
       errorBody: const Text('errorrrr'),
     );
   }
--- client/lib/app/views/bookings/bookings.widgets.dart
+++ client/lib/app/views/bookings/bookings.widgets.dart
@@ -1,1 +1,27 @@
-class BookingsWidgets {}
+import 'package:client/app/widgets/custom_appbar.dart';
+import 'package:client/app/widgets/tabbar/custom_tabbar.dart';
+import 'package:client/core/constans/text_constants.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'widgets/tabbar_widgets.dart';
+
+class BookingsWidgets {
+  Widget body() {
+    return Column(
+      children: [
+        CustomTabBar(
+          tabBarList: tabBarList,
+          tabBarViewList: tabBarViewList,
+        ),
+      ],
+    );
+  }
+
+  appBar() {
+    return CustomAppbar(
+        leading: Text(
+      "Your Bookings",
+      style: TextConstants.instance.heading6,
+    ));
+  }
+}
