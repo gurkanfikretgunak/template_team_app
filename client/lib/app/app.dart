65e8eb2ad3a88d167e48b26cf31775f8073f62d0
Aysenur Perker <nurperker15@gmail.com>
2023-01-17 17:22:32 +0300

    fix : common extension

--- client/lib/app/app.dart
+++ client/lib/app/app.dart
@@ -1,12 +1,7 @@
-import 'package:client/app/views/auth/required_mobile/rquired_mobile.view.dart';
-import 'package:client/app/views/auth/sign_in/sign_in.view.dart';
-import 'package:client/app/views/auth/sign_up/sign_up.view.dart';
-import 'package:client/app/views/auth/verify_mobile/otp_input.view.dart';
-import 'package:client/app/views/auth/verify_mobile/widget/otp_input.dart';
+import 'package:client/app/views/shop/shop.view.dart';
 import 'package:client/core/provider/multi_provider_init.dart';
 import 'package:client/core/theme/custom_theme.dart';
 import 'package:flutter/material.dart';
-
 import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
@@ -17,7 +12,7 @@ class App extends StatelessWidget {
     return MultiProvider(
       providers: MultiProviderInit().providers,
       child: MaterialApp(
-        home: const OtpInputView(),
+        home: const ShopView(),
         title: "Survey App",
         theme: CustomTheme.customLightTheme(context),
         debugShowCheckedModeBanner: false,
