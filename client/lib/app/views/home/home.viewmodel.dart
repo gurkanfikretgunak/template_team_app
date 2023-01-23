3a7db9b0563e8b2384a1d1e9d0e99bf388a7fae6
metecoban <64808380+metecoban@users.noreply.github.com>
2023-01-23 16:22:15 +0300

    fix: Location feature in home

--- client/lib/app/views/home/home.viewmodel.dart
+++ client/lib/app/views/home/home.viewmodel.dart
@@ -2,13 +2,16 @@ import 'package:client/core/base/view_model/base_view_model.dart';
 
 class HomeViewModel extends BaseViewModel {
   List<String> locationList = <String>[
-    'Munich Center',
-    'Moosach',
-    'Pasing-Obermenzing',
-    'Trudering/Riem'
+    'Adana',
+    'Adıyaman',
+    'Afyon',
+    'Ağrı',
+    'Amasya',
+    'Ankara',
+    'Antalya'
   ];
 
-  String _ddLocationValue = 'Munich Center';
+  String _ddLocationValue = 'Antalya';
   String get ddLocationValue => _ddLocationValue;
   set ddLocationValue(newValue) {
     _ddLocationValue = newValue;
