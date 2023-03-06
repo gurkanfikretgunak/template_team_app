b4e3bac5cfe5048603aad429de51cb0eb8fc1834
metecoban <64808380+metecoban@users.noreply.github.com>
2023-03-06 15:15:47 +0300

    add: Retrofit service

---
+++ client/lib/core/services/retrofit/retrofit_service.dart
@@ -0,0 +1,56 @@
+import 'package:client/core/model/login/user_login_request.dart';
+import 'package:client/core/model/login/user_login_response.dart';
+import 'package:client/core/model/register/user_register_request.dart';
+import 'package:client/core/model/register/user_register_response.dart';
+import 'package:client/core/services/retrofit/rest_client.dart';
+import 'package:dio/dio.dart';
+
+class RetrofitService {
+  static final RetrofitService _instance = RetrofitService._init();
+  static RetrofitService get instance => _instance;
+  RetrofitService._init();
+
+  final client = RestClient(Dio());
+
+  Future<UserRegisterResponse> register(
+    String name,
+    String surname,
+    String email,
+    String password,
+    String phoneNumber,
+  ) async {
+    try {
+      UserRegisterResponse response = await client.register(UserRegisterRequest(
+          email: email,
+          password: password,
+          surname: surname,
+          name: name,
+          phoneNumber: phoneNumber));
+
+      //TODO: Add tokens to local storage
+
+      return response;
+    } catch (e) {
+      return UserRegisterResponse(
+        success: false,
+        message: e.toString(),
+      );
+    }
+  }
+
+  Future<UserLoginResponse> login(String email, String password) async {
+    try {
+      UserLoginResponse response = await client
+          .login(UserLoginRequest(email: email, password: password));
+
+      //TODO: Add tokens to local storage
+
+      return response;
+    } catch (e) {
+      return UserLoginResponse(
+        success: false,
+        message: e.toString(),
+      );
+    }
+  }
+}
