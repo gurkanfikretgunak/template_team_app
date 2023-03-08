48ac9af42bcfd3f35e100f57b2b3da29729e5587
metecoban <64808380+metecoban@users.noreply.github.com>
2023-03-09 02:30:13 +0300

    add: multi bloc init created and used on app file

--- client/lib/app/app.dart
+++ client/lib/app/app.dart
@@ -2,11 +2,11 @@ import 'package:client/app/l10n/app_l10n.dart';
 import 'package:client/app/routes/app_routes.dart';
 import 'package:client/app/routes/navigation_service.dart';
 import 'package:client/app/views/auth/sign_up/sign_up.view.dart';
-import 'package:client/app/views/splash/splash.view.dart';
+import 'package:client/core/bloc/multi_bloc_init.dart';
 import 'package:client/core/init/theme/custom_theme.dart';
 import 'package:client/core/provider/multi_provider_init.dart';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
@@ -15,17 +15,19 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: MultiProviderInit().providers,
-      child: MaterialApp(
-        home: const SignUpView(),
-        title: "Salony",
-        theme: CustomTheme.customLightTheme(context),
-        localizationsDelegates: L10n.localizationsDelegates,
-        supportedLocales: L10n.supportedLocales,
-        debugShowCheckedModeBanner: false,
-        onGenerateRoute: AppRoutes.instance.onGenerateRoute,
-        navigatorKey: NavigationService.instance.navigatorKey,
-      ),
-    );
+        providers: MultiProviderInit().providers,
+        child: MultiBlocProvider(
+          providers: MultiBlocInit().blocs,
+          child: MaterialApp(
+            home: const SignUpView(),
+            title: "Salony",
+            theme: CustomTheme.customLightTheme(context),
+            localizationsDelegates: L10n.localizationsDelegates,
+            supportedLocales: L10n.supportedLocales,
+            debugShowCheckedModeBanner: false,
+            onGenerateRoute: AppRoutes.instance.onGenerateRoute,
+            navigatorKey: NavigationService.instance.navigatorKey,
+          ),
+        ));
   }
 }
